5f896aca770bf55bac2960a44df0142ffd000f15
Michael Aldridge <aldridge.mac@gmail.com>
2014-01-05 21:55:35 -0600

    put all report code into class made code look generally more like good python

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ import json
 import logging
 from minecraft_query import MinecraftQuery
 
-def init():
-    logging.basicConfig(level = logging.INFO)
-    sfile = open("servers.json")
-    serverlist = json.load(sfile)
-    sfile.close()
-    return serverlist
+class reportGenerator():
+    def __init__(self, sfilename):
+        logging.basicConfig(level = logging.INFO)
+        sfile = open(sfilename)
+        self.serverlist = json.load(sfile)
+        sfile.close()
 
-def query(hosts):
-    logging.debug("Beginning query cycle")
-    for server in hosts:
-        logging.info("Querying %s", str(server))
-        query = MinecraftQuery(hosts[server]["ip"], hosts[server]["port"])
-        hosts[server]["status"]=query.get_status()
-        logging.debug(json.dumps(hosts[server]["status"]))
-    return hosts
+    def _query(self, hosts):
+        logging.debug("Beginning query cycle")
+        for server in hosts:
+            logging.info("Querying %s", str(server))
+            query = MinecraftQuery(hosts[server]["ip"], hosts[server]["port"])
+            hosts[server]["status"]=query.get_status()
+            logging.debug(json.dumps(hosts[server]["status"]))
+        return hosts
 
-def main(servers):
-    logging.debug("Starting update cycle")
-    report = query(servers)
-    out = open("report.json", 'w')
-    logging.info("Writting report")
-    json.dump(report, out, indent=2)
-    out.close()
+    def update(self):
+        logging.debug("Starting update cycle")
+        report = self._query(self.serverlist)
+        out = open("report.json", 'w')
+        logging.info("Writting report")
+        json.dump(report, out, indent=2)
+        out.close()
 
 if __name__ == "__main__":
-    servers = init()
-    main(servers)
+    reporter = reportGenerator("servers.json")
+    reporter.update()
