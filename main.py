692b11a085e0b406ae7fa02a76449eaeef2cb6e7
Michael Aldridge <aldridge.mac@gmail.com>
2014-01-05 21:33:55 -0600

    now generates json reports for multiple servers given a configuration file

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ from minecraft_query import MinecraftQuery
 
 def init():
     logging.basicConfig(defaultLevel = logging.INFO)
+    sfile = open("servers.json")
+    serverlist = json.load(sfile)
+    sfile.close()
+    return serverlist
 
-def main():
+def query(hosts):
+    logging.debug("Beginning query cycle")
+    for host in hosts:
+        logging.info("Querying %s", str(host))
+        query = MinecraftQuery(hosts[host]["ip"], hosts[host]["port"])
+        hosts[host]["status"]=query.get_status()
+    return hosts
+
+def main(servers):
     logging.debug("Beginning query")
-    query = MinecraftQuery("10.0.0.36", 25565)
-    basic_status = query.get_status()
-    print "The server has %d players" % (basic_status['numplayers'])
-    print basic_status
-
+    report = query(servers)
+    print report
+    out = open("report.json", 'w')
+    json.dump(report, out, indent=2)
+    out.close()
 
 if __name__ == "__main__":
-    init()
-    main()
+    servers = init()
+    main(servers)
